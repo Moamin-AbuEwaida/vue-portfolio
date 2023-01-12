4d9d22028901ce20b2d3d8e4c498ba25f8f0f272
Moamin <moamin.salamah@gmail.com>
2023-01-12 15:48:01 +0100

    adding dist folder

---
+++ js/about.636df1ad.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_portfolio"]=self["webpackChunkvue_portfolio"]||[]).push([[443],{6506:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var e=t(3396);const o={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),a=[r];function s(n,u){return(0,e.wg)(),(0,e.iD)("div",o,a)}var i=t(89);const c={},f=(0,i.Z)(c,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.636df1ad.js.map
